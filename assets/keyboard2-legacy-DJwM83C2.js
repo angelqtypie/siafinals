c8f594ff20d21dbc5eaaeb1ad6d4be3f120b09ce
angelqtypie <angeljustineballaso07@gmail.com>
2025-05-11 15:08:37 +0800

    Updates

--- assets/keyboard2-legacy-DJwM83C2.js
+++ assets/keyboard2-legacy-DJwM83C2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CmvxhzKY.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-C6Pv9QBf.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
